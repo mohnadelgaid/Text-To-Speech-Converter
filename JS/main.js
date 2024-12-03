e7e95c67096d43f275581b6e5eeb4e89e63ab91d
Mohnad_Elgaid <46262625+mohnadelgaid@users.noreply.github.com>
2024-12-03 03:41:01 +0200

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,29 @@
+let speech= new SpeechSynthesisUtterance();
+let button=document.querySelector("button");
+let texterea=document.querySelector("textarea");
+let voiceSelect = document.querySelector("select");
+
+let voices=[];
+window.speechSynthesis.onvoiceschanged=()=>{
+    voices=window.speechSynthesis.getVoices();
+    speech.voice=voices[0];
+
+    voices.forEach((voice,i)=>(
+      voiceSelect.options[i]=new Option(voice.name,i) 
+      )
+    );
+};
+
+voiceSelect.addEventListener("change",()=>{
+    speech.voice=voices[voiceSelect.value];
+});
+
+button.addEventListener("click",()=>{
+    speech.text=texterea.value;
+    window.speechSynthesis.speak(speech);
+    
+    
+});
+
+
+
